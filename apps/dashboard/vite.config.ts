7bc1af91aa50f2a726738448ab84a410e42a76ab
gkibria <gkibria121@gmail.com>
2026-02-25 16:29:05 +0600

    wip

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
+    allowedHosts: ['flowos.gkibria121.com'],
     port: 5173,
     host: true, // Needed for Docker
     proxy: {
